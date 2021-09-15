3eb1cda9449c148e0abc4ee010c3ecb47b573dde
AE201102 <ejy0223@etoos.com>
2021-09-15 17:29:51 +0900

    [feature] 차트 기능 결합 및 응용
    - 이벤트 제어
    - 비동기 처리
    - props로 dataset 통신

--- chart-library/src/components/BarChart.vue
+++ chart-library/src/components/BarChart.vue
@@ -3,9 +3,8 @@
 </template>
 
 <script>
-
-
 export default {
+  props:[ 'propsdata' ],
   mounted(){
     // 인스턴트가 화면에 붙고 나서 실행되어야하믄로 mouted 훅 사용
     //var ctx = document.getElementById('barChart');
@@ -13,27 +12,7 @@ export default {
         type: 'bar',
         data: {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+            datasets: this.propsdata
         },
         options: {
           plugins: {
@@ -46,6 +25,10 @@ export default {
             y: {
                 beginAtZero: true
             }
+          },
+          // 이벤트 속성 (예시)
+          onClick: function(){
+            this.$emit('refresh:chart')
           }
         }
     });
