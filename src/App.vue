b8912fa5295aaf6755e5ad6935d3953bdb847c36
AE201102 <ejy0223@etoos.com>
2021-09-16 14:14:47 +0900

    [feature] slot의 확장성과 활용성

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,52 @@
 <template>
   <div>
-    <!-- slot 사용  -->
-    <!-- <ul>
-      <item>아이템 1</item>
-      <item>아이템 2</item>
-      <item>아이템 3</item>
-      <item>아이템 4</item>
-      <item>아이템 5</item>
-    </ul> -->
-
-    <!-- slot 사용 안함  -->
-    <ul>
-       <not-slot-item v-for="(item, index) in items" :key="index" :item="item"></not-slot-item>
-    </ul>
+    <div>
+      <h2> slot을 이용하지 않은 방법</h2>
+      <ul>
+        <not-slot-item v-for="(item, index) in items" :key="index" :item="item"></not-slot-item>
+      </ul>
+    </div>
+    <div>
+      <h2>slot을 사용한 벙법</h2>
+      <ul>
+        <item v-for="(item, index) in items" :key="index">{{ item }}</item>
+      </ul>
+    </div>
+    <div>
+      <h2>slot의 유용한 활용</h2>
+      <item>
+        아이템1
+      </item>
+      <item>
+        <h3>버튼 추가</h3>
+        <div>
+          아이템 2
+          <button type="button">click</button>
+        </div>
+      </item>
+      <item>
+        <h3>이미지 추가</h3>
+        <div>
+          아이템3
+        </div>
+        <img src="./assets/endgame.png" alt="엔드게임" style="width: 100px">
+      </item>
+      <item>
+        <h3>스타일을 상위 컴포넌트에서 지정 가능</h3>
+        <div style="color: blue; font-size: 30px;">
+          아이템4
+        </div>
+      </item>
+    </div>
   </div>
 </template>
 
 <script>
-//import Item from './components/Item.vue';
+import Item from './components/Item.vue';
 import NotSlotItem from './components/NotSlotItem'
 export default {
   components: {
-    //Item,
+    Item,
     NotSlotItem
   },
   data(){
